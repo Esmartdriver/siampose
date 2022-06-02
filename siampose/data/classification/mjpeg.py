ae8174da9e88361c1536b15fb739f0a0465ae200
Raphael Jean <rjean@esmartcontrol.com>
2022-06-02 11:23:42 -0400

    Support for labels

--- siampose/data/classification/mjpeg.py
+++ siampose/data/classification/mjpeg.py
@@ -3,7 +3,7 @@ from re import I
 from pytest import param
 import torch
 import glob
-import os 
+import os
 from natsort import natsorted
 import cv2
 import os
@@ -15,53 +15,82 @@ import torchvision.transforms as T
 from torch.utils.data.dataloader import DataLoader
 from PIL import Image
 
+IMAGENET_MEAN_STD = [[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]]
+
+
 class MjpegDataset(torch.utils.data.Dataset):
-    
-    def __init__(self, path: str, transform=None, single_image_mode=False, crop_height=224, crop_width=224, min_scale=0.6, max_scale=1.6, only_crops=True) -> None:
-        self.unlabelled_files = natsorted(glob.glob(os.path.join(path, "unlabelled/*.jpg")))
+    def __init__(
+        self,
+        path: str,
+        transform=None,
+        single_image_mode=False,
+        crop_height=224,
+        crop_width=224,
+        min_scale=0.6,
+        max_scale=1.6,
+        only_crops=True,
+        include_labelled=False,
+        labelled_only=False,
+    ) -> None:
+        if labelled_only:
+            self.unlabelled_files = []
+        else:
+            self.unlabelled_files = natsorted(glob.glob(os.path.join(path, "unlabelled/*.jpg")))
+        self.files = []
+        self.labelled_files = {}
+        if include_labelled:
+            for folder in natsorted(glob.glob(os.path.join(path, "*"))):
+                if os.path.isdir(folder):
+                    category = folder.split("/")[-1]
+                    if category == "unlabelled":
+                        continue
+                    self.labelled_files[category] = natsorted(glob.glob(os.path.join(path, f"{category}/*.jpg")))
+
         self.single_image_mode = single_image_mode
         self.file_map = {}
         self.crop_width, self.crop_height = crop_width, crop_height
         self.min_scale = min_scale
         self.max_scale = max_scale
         self.only_crops = only_crops
+        # self.labelled_only = labelled_only
+        self._build_index()
         self._build_sequence_id_map()
         self._build_file_map()
+
         self.transform = transform
 
-    def random_video_crop(self,img, xc, yc):
+    def _build_index(self):
+        self.files = self.unlabelled_files
+        for category in self.labelled_files:
+            self.files += self.labelled_files[category]
+
+    def random_video_crop(self, img, xc, yc):
         if not isinstance(img, np.ndarray):
             h, w = img.height, img.width
         else:
-            h, w, _= img.shape
-        crop_w = int(np.clip(np.random.uniform(self.crop_width*self.min_scale, self.crop_width*self.max_scale), 0, h))
-        crop_h = int(np.clip(np.random.uniform(self.crop_height*self.min_scale, self.crop_height*self.max_scale), 0, w))
-        x1, y1 = int(np.clip(xc-crop_w/2, 0, w-1)), int(np.clip(yc-crop_h/2, 0, h-1))
-        x2, y2 = int(np.clip(xc+crop_w/2, 0, w-1)), int(np.clip(yc+crop_h/2, 0, h-1))
+            h, w, _ = img.shape
+        crop_w = int(np.clip(np.random.uniform(self.crop_width * self.min_scale, self.crop_width * self.max_scale), 0, h))
+        crop_h = int(np.clip(np.random.uniform(self.crop_height * self.min_scale, self.crop_height * self.max_scale), 0, w))
+        x1, y1 = int(np.clip(xc - crop_w / 2, 0, w - 1)), int(np.clip(yc - crop_h / 2, 0, h - 1))
+        x2, y2 = int(np.clip(xc + crop_w / 2, 0, w - 1)), int(np.clip(yc + crop_h / 2, 0, h - 1))
         if isinstance(img, np.ndarray):
-            crop = img[y1:y2,x1:x2]
-        else: # Assume Pillow Image
+            crop = img[y1:y2, x1:x2]
+        else:  # Assume Pillow Image
             img: Image.Image
-            crop = img.crop([x1,y1, x2, y2])
+            crop = img.crop([x1, y1, x2, y2])
         return crop
 
     def __len__(self):
-        return len(self.unlabelled_files)
+        return len(self.files)
 
     def _build_sequence_id_map(self):
-        self.sequence_id_map={}
-        for file in self.unlabelled_files:
+        self.sequence_id_map = {}
+        for file in self.files:
             sequence_id = self._get_sequence_id(file)
             if sequence_id not in self.sequence_id_map:
                 self.sequence_id_map[sequence_id] = [file]
             else:
                 self.sequence_id_map[sequence_id].append(file)
-    
-    def _fast_read(self, filename):
-        assert False
-        img = cv2.imread(filename)
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return img_rgb
 
     def _build_file_map(self):
         self.file_map = {}
@@ -73,64 +102,80 @@ class MjpegDataset(torch.utils.data.Dataset):
                 "sequence_id": sequence_id,
                 "frame_number": self._get_frame_number(file),
                 "frame_index": self.sequence_id_map[sequence_id].index(file),
-                "idx": idx
+                "idx": idx,
             }
+
     def _get_sequence_id(self, filename):
         basename = os.path.basename(filename)
-        sequence_id = "_".join(basename.split("_")[0:-1])
+        category = self._get_category(filename)
+        if category == "unlabelled":
+            sequence_id = "_".join(basename.split("_")[0:-1])
+        else:  # Remove the "object ID" also
+            sequence_id = "_".join(basename.split("_")[0:-2])
         return sequence_id
 
     def _get_frame_number(self, filename):
         basename = os.path.basename(filename)
-        frame_number = basename.split("_")[-1].replace(".jpg","")
+        category = self._get_category(filename)
+        if category == "unlabelled":
+            frame_number = basename.split("_")[-1].replace(".jpg", "")
+        else:  # Remove the "object ID" also
+            frame_number = basename.split("_")[-2].replace(".jpg", "")
+
         return int(frame_number)
 
     def get_single_item(self, idx):
-        filename = self.unlabelled_files[idx]
+        filename = self.files[idx]
         return self.get_single_file(filename)
 
     def get_single_file(self, filename):
-        #img = self._fast_read(filename)
+        # img = self._fast_read(filename)
         img = Image.open(filename)
         sample = {
-            "image" : img,
+            "image": img,
             "sequence": self._get_sequence_id(filename),
             "path": filename,
-            "frame_numer": self._get_frame_number(filename)
+            "frame_numer": self._get_frame_number(filename),
+            "category": self._get_category(filename),
         }
         return sample
 
+    def _get_category(self, filename):
+        return filename.split("/")[-2]
+
     def get_frame_index(self, filename):
         basename = os.path.basename(filename)
-        frame_index= self.file_map[basename]["frame_index"]
+        frame_index = self.file_map[basename]["frame_index"]
         return frame_index
 
     def _get_nearby_frame_index(self, filename, min_dist=1, max_dist=5, both_ways=True):
         basename = os.path.basename(filename)
-        frame_index= self.file_map[basename]["frame_index"]
+        frame_index = self.file_map[basename]["frame_index"]
         gap = random.randint(min_dist, max_dist)
         sequence_id = self._get_sequence_id(filename)
         if both_ways and random.random() > 0.5:
             gap = -gap
-        assert gap!=0
-        last_index = len(self.sequence_id_map[sequence_id])-1
-        nearby_frame_index = np.clip(frame_index+gap, 1, last_index)
-        if nearby_frame_index==frame_index:
-            nearby_frame_index = np.clip(frame_index-gap, 1, last_index)
-        return nearby_frame_index 
+        assert gap != 0
+        last_index = len(self.sequence_id_map[sequence_id]) - 1
+        nearby_frame_index = np.clip(frame_index + gap, 1, last_index)
+        if nearby_frame_index == frame_index:
+            nearby_frame_index = np.clip(frame_index - gap, 1, last_index)
+        return nearby_frame_index
 
     @property
     def seq_subset(self):
         return natsorted(list(self.sequence_id_map))
 
     def random_normal_center(self, w, h, std_dev=3):
-        xc = int(np.clip(np.random.normal(w/2, w/std_dev), 0, w-1))
-        yc = int(np.clip(np.random.normal(h/2, h/std_dev), 0, h-1))
+        xc = int(np.clip(np.random.normal(w / 2, w / std_dev), 0, w - 1))
+        yc = int(np.clip(np.random.normal(h / 2, h / std_dev), 0, h - 1))
         return xc, yc
 
     def __getitem__(self, idx):
         base_sample = self.get_single_item(idx)
         if self.single_image_mode:
+            if self.transform:
+                base_sample["image"] = self.transform(base_sample["image"])
             return base_sample
         else:
             # Crop pairs!
@@ -138,7 +183,7 @@ class MjpegDataset(torch.utils.data.Dataset):
 
             img1 = sample["image"]
             if isinstance(img1, np.ndarray):
-                h, w, c= img1.shape
+                h, w, c = img1.shape
             else:
                 img1: Image.Image
                 h, w = img1.height, img1.width
@@ -148,15 +193,15 @@ class MjpegDataset(torch.utils.data.Dataset):
             nearby_filename = self.sequence_id_map[sample["sequence"]][nearby_index]
             img2 = Image.open(nearby_filename)
             crop2 = self.random_video_crop(img2, xc, yc)
-            crops =[crop1, crop2]
+            crops = [crop1, crop2]
             if self.transform:
                 crops = [self.transform(crop) for crop in crops]
-            assert len(crops)==2
+            assert len(crops) == 2
             sample["crops"] = crops
             if self.only_crops:
                 del sample["image"]
             return sample
-IMAGENET_MEAN_STD = [[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]]
+
 
 class MjpegDataModule(pytorch_lightning.LightningDataModule):
     def __init__(
@@ -167,6 +212,7 @@ class MjpegDataModule(pytorch_lightning.LightningDataModule):
         num_workers=8,
         pairing="next",
         dryrun=False,
+        labelled_only=False,  # Usefull when generating embeddings
     ):
         super().__init__()
         self.data_dir = data_dir
@@ -180,9 +226,7 @@ class MjpegDataModule(pytorch_lightning.LightningDataModule):
     def setup(self, stage=None):
         if not self.issetup:
             self.train_transform = self.get_transform(self.image_size)
-            self.eval_transform = self.get_transform(
-                self.image_size, evaluation=True
-            )
+            self.eval_transform = self.get_transform(self.image_size, evaluation=True)
             self.train_dataset = MjpegDataset(
                 os.path.join(self.data_dir, "training"),
                 transform=self.train_transform,
@@ -199,44 +243,40 @@ class MjpegDataModule(pytorch_lightning.LightningDataModule):
             self.valid_sample_count = len(self.val_dataset)
             self.issetup = True
 
-
-    def get_transform(
-        self, image_size, mean_std=IMAGENET_MEAN_STD, evaluation=False
-    ):
+    def get_transform(self, image_size, mean_std=IMAGENET_MEAN_STD, evaluation=False):
         if not evaluation:
             # hflip = T.RandomHorizontalFlip()
             p_blur = 0.5 if image_size > 32 else 0
             transform_list = [
                 # T.RandomResizedCrop(image_size, scale=(0.2, 1.0)),
                 # hflip,
-                # 
-                T.Resize((image_size,image_size)),
+                #
+                T.Resize((image_size, image_size)),
                 T.RandomApply([T.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
                 T.RandomGrayscale(p=0.2),
                 T.RandomApply(
-                    [
-                        T.GaussianBlur(
-                            kernel_size=image_size // 20 * 2 + 1, sigma=(0.1, 2.0)
-                        )
-                    ],
+                    [T.GaussianBlur(kernel_size=image_size // 20 * 2 + 1, sigma=(0.1, 2.0))],
                     p=p_blur,
                 ),
                 T.ToTensor(),
                 T.Normalize(*mean_std),
-                
             ]
             return T.Compose(transform_list)
         else:
-            return T.Compose([  T.Resize((image_size,image_size)), T.ToTensor(), T.Normalize(*mean_std), ])
+            return T.Compose(
+                [
+                    T.Resize((image_size, image_size)),
+                    T.ToTensor(),
+                    T.Normalize(*mean_std),
+                ]
+            )
 
     def train_dataloader(self, evaluation=False) -> DataLoader:
         self.train_dataset.transform = self.train_transform
         if evaluation:
             self.train_dataset.transform = self.eval_transform
             self.train_dataset.memory = True  # Avoid horizontal flip for evaluation.
-        if (
-            self.dryrun
-        ):  # Just to quickly test the training loop. Trains on "test set", validation on valid set.
+        if self.dryrun:  # Just to quickly test the training loop. Trains on "test set", validation on valid set.
             print("WARNING: DRY RUN. Not performing real training.")
             return self.test_dataloader()
         return DataLoader(
@@ -258,6 +298,4 @@ class MjpegDataModule(pytorch_lightning.LightningDataModule):
         )
 
     def test_dataloader(self) -> DataLoader:
-        return DataLoader(
-            self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers
-        )
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
--- test/test_mjpeg_dataloader.py
+++ test/test_mjpeg_dataloader.py
@@ -1,11 +1,17 @@
 from calendar import c
+from multiprocessing.context import assert_spawning
+
+from pytest import skip
 from siampose.data.classification.mjpeg import MjpegDataset, MjpegDataModule
 import numpy as np
 import torch
 import os
 import cv2
 from PIL import Image
-INTERACTIVE=False
+
+INTERACTIVE = False
+
+
 def test_test():
     assert True
 
@@ -16,32 +22,37 @@ def test_random_video_crop():
     if not isinstance(img, np.ndarray):
         h, w = img.height, img.width
     else:
-        h, w , c = img.shape
-    crop = dataset.random_video_crop(img, w//2, h//2)
+        h, w, c = img.shape
+    crop = dataset.random_video_crop(img, w // 2, h // 2)
     if not isinstance(img, np.ndarray):
         crop_w, crop_h = crop.width, crop.height
     else:
         crop_h, crop_w, _ = crop.shape
-        
-    assert dataset.min_scale*crop_w < crop_w < dataset.max_scale*crop_w
-    assert dataset.min_scale*crop_h < crop_h < dataset.max_scale*crop_h
+
+    assert dataset.min_scale * crop_w < crop_w < dataset.max_scale * crop_w
+    assert dataset.min_scale * crop_h < crop_h < dataset.max_scale * crop_h
+
 
 def test_dataset_get_sequence_id_and_frame_number():
     dataset = MjpegDataset("test/data/mjpeg/training", single_image_mode=True)
-    sequence_id = dataset._get_sequence_id("long_sequence_name_00005.jpg")
-    assert sequence_id =="long_sequence_name"
-    frame_number = dataset._get_frame_number("some_string_with_underscores_12345.jpg")
+    sequence_id = dataset._get_sequence_id("unlabelled/long_sequence_name_00005.jpg")
+    assert sequence_id == "long_sequence_name"
+    frame_number = dataset._get_frame_number("bogus_category/some_string_with_underscores_12345_6.jpg")
     assert frame_number == 12345
+
 
 def test_get_nearby_frame_index():
     dataset = MjpegDataset("test/data/mjpeg/training", single_image_mode=True)
     filename = "test/data/mjpeg/training/unlabelled/20210723_172608_031446.jpg"
     idx = dataset.get_frame_index(filename)
     seq_id = dataset._get_sequence_id(filename)
-    nearby_idx = dataset._get_nearby_frame_index("test/data/mjpeg/training/unlabelled/20210723_172608_031446.jpg", min_dist=1, max_dist=1)
-    assert idx!=nearby_idx
-    assert abs(idx-nearby_idx) <=1
-    assert dataset.sequence_id_map[seq_id][dataset.get_frame_index(filename)]==filename
+    nearby_idx = dataset._get_nearby_frame_index(
+        "test/data/mjpeg/training/unlabelled/20210723_172608_031446.jpg", min_dist=1, max_dist=1
+    )
+    assert idx != nearby_idx
+    assert abs(idx - nearby_idx) <= 1
+    assert dataset.sequence_id_map[seq_id][dataset.get_frame_index(filename)] == filename
+
 
 def test_dataset_unlabelled_single_mode():
     dataset = MjpegDataset("test/data/mjpeg/training", single_image_mode=True, only_crops=False)
@@ -50,8 +61,8 @@ def test_dataset_unlabelled_single_mode():
     assert "image" in dataset[0]
     assert "sequence" in dataset[0]
     assert dataset[0]["sequence"] == "20210723_172608"
-    assert os.path.basename(dataset[0]["path"])!= dataset[0]["path"]
-    assert len(dataset.sequence_id_map)==4
+    assert os.path.basename(dataset[0]["path"]) != dataset[0]["path"]
+    assert len(dataset.sequence_id_map) == 4
     assert len(dataset.file_map) == len(dataset)
 
     for sample in dataset:
@@ -60,33 +71,99 @@ def test_dataset_unlabelled_single_mode():
         if INTERACTIVE:
             cv2.imshow("test", sample["image"])
             cv2.waitKey(1)
+
+
 import torchvision.transforms as transforms
 
-def test_dataset_unlabelled():
 
+def test_dataset_unlabelled():
     dataset = MjpegDataset("test/data/mjpeg/training", transform=None)
     assert len(dataset.unlabelled_files) > 0
     assert len(dataset) == 11
     sample = dataset[0]
     assert "sequence" in sample
     assert "crops" in sample
-    crop1, crop2 =  sample["crops"]
+    crop1, crop2 = sample["crops"]
     assert crop1.width != crop2.width
     assert isinstance(crop1, Image.Image)
-    transform = transforms.Compose([
-         transforms.ToTensor(),
-         transforms.Resize((224,224)),
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Resize((224, 224)),
+        ]
+    )
     dataset = MjpegDataset("test/data/mjpeg/training", transform=transform)
     assert isinstance(dataset.seq_subset, list)
     assert len(dataset.seq_subset) > 0
     assert isinstance(dataset.seq_subset[0], str)
     sample = dataset[0]
-    crop1, crop2 =  sample["crops"]
+    crop1, crop2 = sample["crops"]
+    assert sample["category"] == "unlabelled"
     assert isinstance(crop1, torch.Tensor)
-    assert crop1.shape==crop2.shape
-    assert crop1.shape==(3, 224,224)
+    assert crop1.shape == crop2.shape
+    assert crop1.shape == (3, 224, 224)
 
+
+def test_dataset_labelled():
+    dataset = MjpegDataset("test/data/mjpeg/training", transform=None, include_labelled=True, single_image_mode=True)
+    assert len(dataset.unlabelled_files) > 0
+    assert len(dataset) == 17
+    sample = dataset[0]
+    assert "sequence" in sample
+    assert "crops" not in sample
+    assert "image" in sample
+    image = sample["image"]
+    assert isinstance(image, Image.Image)
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Resize((224, 224)),
+        ]
+    )
+    dataset = MjpegDataset("test/data/mjpeg/training", transform=transform, include_labelled=True, single_image_mode=True)
+    assert isinstance(dataset.seq_subset, list)
+    assert len(dataset.seq_subset) > 0
+    assert isinstance(dataset.seq_subset[0], str)
+    sample = dataset[-1]
+    image = sample["image"]
+    assert isinstance(image, torch.Tensor)
+    assert image.shape == (3, 224, 224)
+    assert sample["category"] == "speed_limit"
+
+
+def test_dataset_labelled_only():
+    dataset = MjpegDataset(
+        "test/data/mjpeg/training", transform=None, include_labelled=True, labelled_only=True, single_image_mode=True
+    )
+    assert len(dataset.unlabelled_files) > 0
+    assert len(dataset) == 6
+    sample = dataset[0]
+    assert "sequence" in sample
+    assert "crops" not in sample
+    assert "image" in sample
+    image = sample["image"]
+    assert isinstance(image, Image.Image)
+    for sample in dataset:
+        assert sample["category"] != "unlabelled"
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Resize((224, 224)),
+        ]
+    )
+    dataset = MjpegDataset(
+        "test/data/mjpeg/training", transform=transform, include_labelled=True, labelled_only=True, single_image_mode=True
+    )
+    assert isinstance(dataset.seq_subset, list)
+    assert len(dataset.seq_subset) > 0
+    assert isinstance(dataset.seq_subset[0], str)
+    sample = dataset[-1]
+    image = sample["image"]
+    assert isinstance(image, torch.Tensor)
+    assert image.shape == (3, 224, 224)
+    assert sample["category"] == "speed_limit"
+    for sample in dataset:
+        assert sample["category"] != "unlabelled"
 
 
 def test_mjpeg_data_module():
@@ -101,9 +178,14 @@ def test_mjpeg_data_module():
     for sample in dataloader:
         assert sample["crops"] is not None
         crop1, crop2 = sample["crops"]
-        assert crop1.shape == (11, 3, 224,224)
+        assert crop1.shape == (11, 3, 224, 224)
         assert crop2.shape == (11, 3, 224, 224)
 
+
+import pytest
+
+
+@pytest.mark.skip(reason="Too long to test.")
 def test_mjpeg_data_module_full():
     dm = MjpegDataModule("/home/raphael/esmart/esmart-ai-datasets/data/esmart_mjpeg/")
     dm.setup()
@@ -111,12 +193,12 @@ def test_mjpeg_data_module_full():
     for sample in dm.train_dataset:
         assert sample["crops"] is not None
         crop1, crop2 = sample["crops"]
-        assert crop1.shape==crop2.shape
-        assert crop1.shape==(3,224,224)
+        assert crop1.shape == crop2.shape
+        assert crop1.shape == (3, 224, 224)
 
     dataloader = dm.train_dataloader()
     for sample in dataloader:
         assert sample["crops"] is not None
         crop1, crop2 = sample["crops"]
-        assert crop1.shape == (11, 3, 224,224)
+        assert crop1.shape == (11, 3, 224, 224)
         assert crop2.shape == (11, 3, 224, 224)
